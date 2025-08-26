1f942585a276764ab743711d488a790c329262e5
phpp28 <pstephan99@web.de>
2025-08-26 19:19:33 +0200

    Frontend: rename TextDocument to HighlightText

--- src/main/resources/static/js/utils/getter.js
+++ src/main/resources/static/js/utils/getter.js
@@ -1,7 +1,7 @@
 import BarChart from "/js/charts/BarChart.js";
 import LineChart from "/js/charts/LineChart.js";
 import PieChart from "/js/charts/PieChart.js";
-import TextDocument from "/js/documents/TextDocument.js";
+import HighlightText from "/js/text/HighlightText.js";
 import Map2D from "/js/maps/Map2D.js";
 import Network2D from "/js/networks/Network2D.js";
 
@@ -9,7 +9,7 @@ export default {
   BarChart,
   LineChart,
   PieChart,
-  TextDocument,
+  HighlightText,
   Map2D,
   Network2D,
 };
