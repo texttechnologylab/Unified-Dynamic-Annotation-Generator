eadbbbbacbf0c853699929cd7f69d12e65a92b1e
LociStar <locistar.got@gmail.com>
2025-09-21 17:50:45 +0200

    Enable configuration properties for DbProps in the application

--- src/main/java/uni/textimager/sandbox/App.java
+++ src/main/java/uni/textimager/sandbox/App.java
@@ -2,8 +2,11 @@ package uni.textimager.sandbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import uni.textimager.sandbox.importer.config.DbProps;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DbProps.class)
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
